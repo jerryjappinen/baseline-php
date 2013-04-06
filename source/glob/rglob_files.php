cdcad7938891fc0a0f855bd13e238f84dc3b74a2
Jerry Jäppinen <eiskis@gmail.com>
2013-04-06 16:42:57 +0300

    Accepting multiple file type parameters in glob_files() and rglob_files()

--- source/glob/rglob_files.php
+++ source/glob/rglob_files.php
@@ -13,10 +13,18 @@
 *	...
 */
 function rglob_files ($path = '', $filetypes = array()) {
+
+	// Accept file type restrictions as a single array or multiple independent values
+	$arguments = func_get_args();
+	array_shift($arguments);
+	$filetypes = array_flatten($arguments);
+
+	// Run glob_files for this directory and its subdirectories
 	$files = glob_files($path, $filetypes);
 	foreach (glob_dir($path) as $child) {
 		$files = array_merge($files, rglob_files($child, $filetypes));
 	}
+
 	return $files;
 }
 
